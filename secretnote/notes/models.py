27a9dbff3c1f6c65ad2e9def56c368107cf42de7
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-07-12 18:32:25 +0300

    feat: added view count and resolved its error and migrated

--- secretnote/notes/models.py
+++ secretnote/notes/models.py
@@ -5,6 +5,7 @@ import uuid
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    view_count = models.IntegerField(default=0)
     creation_date = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
--- secretnote/notes/views.py
+++ secretnote/notes/views.py
@@ -13,10 +13,14 @@ def notes(request):
 # @login_required
 def note(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
-    print(datetime.now().day)
-    print(note.creation_date)
+    # print(datetime.now().day)
+    # print(note.creation_date)
+    note.view_count = note.view_count + 1
+    print(note.view_count)
+    note.save()
     if (
-        note.creation_date.year < datetime.now().year
+        note.view_count >= 10
+        or note.creation_date.year < datetime.now().year
         or note.creation_date.month < datetime.now().month
         or note.creation_date.day < datetime.now().day
     ):
@@ -31,4 +35,5 @@ def note(request, note_id):
             "notes/note.html",
             {"note": "", "date": note.creation_date, "current_date": datetime.now()},
         )
+
     return render(request, "notes/note.html", {"note": note})
